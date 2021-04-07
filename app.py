1a43430895a50d97ebf87baf924768b34a873e69
sivasaiyadav8143 <sivasai.yadav@gmail.com>
2021-04-07 17:22:10 +0530

    First

--- app.py
+++ app.py
@@ -4,28 +4,25 @@ import numpy as np
 
 app = Flask(__name__)
 
-model_pk = pickle.load(open("flower.pkl","rb"))
+model_pk = pickle.load(open('flower.pkl','rb'))
 
-@app.route('/api_predict', methods = ['POST','GET'])
+@app.route('/api_predict',methods=["POST","GET"])
 def api_predict():
-    if request.method == 'GET':
-        return "Please Send POST Request"
-    elif request.method == 'POST':
-        
-        print("Hello" + str(request.get_json()))
-        
+    if request.method == "GET":
+        return "Please send a POST request"
+    else:
         data = request.get_json()
         
-        sepal_length = data["sepal_length"]
-        sepal_width = data["sepal_width"]
-        petal_length = data["petal_length"]
-        petal_width = data["petal_width"]
-    
-        data = np.array([[sepal_length, sepal_width, 
-                          petal_length, petal_width]])
-           
-        prediction = model_pk.predict(data)
+        se_le = data['sepal_length']
+        se_wi = data['sepal_width']
+        pe_le = data['petal_length']
+        pe_wi = data['petal_width']
+        
+        input_data = np.array([[se_le,se_wi,pe_le,pe_wi]])
+        prediction = model_pk.predict(input_data)
+        
         return str(prediction)
-
-if __name__ == "__main__":
+    
+if __name__ == '__main__'
     app.run()
+        
